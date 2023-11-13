87d9ffbba235534dcb5d0892837ca80be3601686
pdgjm6785 <pdgjm6785@gmail.com>
2023-11-13 21:21:06 +0900

    update 変数

--- src/Review01.java
+++ src/Review01.java
@@ -3,8 +3,8 @@ public class Review01 {
 
     public static void main(String[] args) {
         // 変数の宣言・代入
-        int shina1 = 100;
-        double zeikin1 = 0.08;
+        int shina1 = 1500;
+        double zeikin1 = 0.1;
 
         //　double型resultを宣言、double型引値を２つ受け取るメソットの定義（戻り値なし）
         double result = taxMethod1(shina1, zeikin1);
