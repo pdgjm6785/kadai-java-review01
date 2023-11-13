e46d63b02c0949128edc5d9e8f2123e118356b57
pdgjm6785 <pdgjm6785@gmail.com>
2023-11-13 21:05:25 +0900

    update cast

---
+++ src/Review01.java
@@ -0,0 +1,21 @@
+
+public class Review01 {
+
+    public static void main(String[] args) {
+        // 変数の宣言・代入
+        int shina1 = 100;
+        double zeikin1 = 0.08;
+
+        //　double型resultを宣言、double型引値を２つ受け取るメソットの定義（戻り値なし）
+        double result = taxMethod1(shina1, zeikin1);
+
+        //　演算結果を表示する
+        System.out.println(shina1 + "円の商品の税込価格は" + (int)(shina1 + result) + "円(消費税は" + (int)result + "円)です。");
+    }
+
+    // double型引数を２つ受け取るメソットの定義（戻り値あり）
+    public static int taxMethod1(int shina1, double zeikin1) {
+        double result = shina1 * zeikin1;
+        return (int) result;
+    }
+}
